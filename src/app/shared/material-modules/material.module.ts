58470207a29c2968439ebc693aff2e993699bb9a
GerardoRod <Gerardo.luis2@gmail.com>
2021-02-18 13:27:19 +0000

    All Mobile Done

--- src/app/shared/material-modules/material.module.ts
+++ src/app/shared/material-modules/material.module.ts
@@ -34,6 +34,9 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 @NgModule({
     exports: [
@@ -70,6 +73,7 @@ import { MatTreeModule } from '@angular/material/tree';
         MatToolbarModule,
         MatTooltipModule,
         MatTreeModule,
+        FlexLayoutModule
     ],
     providers: [
     ]
